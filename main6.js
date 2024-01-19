2f7dabe9fbbfd2f0dc3dbcea57e1260f0a4bf0a5
Popa Teodor <s2400066@BC-C02J61CUQ6LRs-MacBook-Air.local>
2024-01-19 13:41:35 +0200

    Task 2 done

---
+++ main6.js
@@ -0,0 +1,21 @@
+let x, y, z;
+x = 7;
+y = 3;
+
+//sum
+z = x + y;
+console.log(z);
+
+//difference
+z = x - y;
+console.log(z);
+
+//product
+z = x / y;
+console.log(z);
+
+//quotient
+z = x % y;
+console.log(z);
+
+//reminder

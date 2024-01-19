d58cf940f8a350fc4cfe3b2ace9e2e20814706d9
Popa Teodor <s2400066@BC-C02J61CUQ6LRs-MacBook-Air.local>
2024-01-19 13:00:27 +0200

    Task 1, 19.1

---
+++ main5.js
@@ -0,0 +1,13 @@
+const age = 27;
+const name = "Teodor";
+const isStudent = True;
+const hobbies = ["cooking", "tennis"];
+let schoolName = "Business College";
+let grade = "10";
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof schoolName);
+console.log(typeof grade);

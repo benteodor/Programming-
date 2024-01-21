5f7d562ded99348a7180276f3222783a28e8054a
Popa Teodor <teodor.popa96@gmail.com>
2024-01-21 23:13:02 +0200

    Update

---
+++ week 2/numbers_02.js
@@ -0,0 +1,6 @@
+let Fahrenheit = 75;
+let fahrenheitToCelsius = ((Fahrenheit - 32) * 5) / 9;
+let Celsius = Math.round(fahrenheitToCelsius);
+
+console.log(fahrenheitToCelsius);
+console.log(Celsius);

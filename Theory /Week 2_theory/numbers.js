cad5426641bbc318325c9688fcdcd9538817849c
Popa Teodor <s2400066@BC-C02J61CUQ6LRs-MacBook-Air.local>
2024-01-21 20:01:53 +0200

    Update

--- Theory /Week 2_theory/numbers.js
+++ Theory /Week 2_theory/numbers.js
@@ -18,9 +18,16 @@ let numericValue = Number.parseInt(str, 10);
 console.log(numericValue);
 */
 
-let str = "20";
+/*let str = "20";
 let numericValue = Number.parseInt(str, 10);
-console.log(numericValue);
+console.log(numericValue);*/
 
 //Number.parseInt(string, radix);
 // The first parameter is the string that you'd like to convert into a number. The second argument is the radix that will be used in the conversion. The radix is the base of the numerical system that you'd like to use. For most use cases the radix you'd like to use is 10 which represents the decimal system.
+
+// Exercise (cases for converting to a number)
+let a = 10;
+let b = "30";
+b = Number.parseInt(b, 10);
+let sum = a + b;
+console.log(sum);

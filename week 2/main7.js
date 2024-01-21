6c33a6f6f67594b7671a17143688071ff597b6e9
Popa Teodor <teodor.popa96@gmail.com>
2024-01-21 22:57:13 +0200

    Update

--- week 2/main7.js
+++
@@ -1,3 +0,0 @@
-let number = 42;
-let stringnumber = number.toString();
-console.log(stringnumber);
---
+++ week 2/numbers_01.js
@@ -0,0 +1,27 @@
+// Complete the function convertNumberToString such that it converts the number it receives into a string.
+
+/*let convertNumberToString = 42;
+let stringAnswer = convertNumberToString.toString();
+console.log(stringAnswer);*/
+
+/*let convertNumberToString = 97;
+let stringAnswer = convertNumberToString.toString();
+console.log(stringAnswer);*/
+
+/*let convertNumberToString = 11;
+let stringAnswer = convertNumberToString.toString();
+console.log(stringAnswer);*/
+
+// Complete the function convertStringToNumber such that it converts the string it receives into a number.
+
+/*let convertStringToNumber = "42";
+let numericValue = Number.parseInt(convertStringToNumber);
+console.log(numericValue);*/
+
+/*let convertStringToNumber = "97";
+let numericValue = Number.parseInt(convertStringToNumber);
+console.log(numericValue);*/
+
+let convertStringToNumber = "11";
+let numericValue = Number.parseInt(convertStringToNumber);
+console.log(numericValue);

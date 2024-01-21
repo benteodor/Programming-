c8633a6529a800d4355a23652a946b05041f1e75
Popa Teodor <s2400066@BC-C02J61CUQ6LRs-MacBook-Air.local>
2024-01-21 19:33:21 +0200

    Update

---
+++ Theory /Basic functions/data_types.js
@@ -0,0 +1,22 @@
+/*let name = "Alice";
+let age = 30;
+let isStudent = false;
+let hobbies = ["reading", "cycling"];
+
+console.log(typeof name); // "string"
+console.log(typeof age); // "number"
+console.log(typeof isStudent); // "boolean"
+console.log(typeof hobbies); // "object" (arrays are type 'object' in JavaScript)
+*/
+
+let name = "Teodor";
+let age = 28;
+let isStudent = true;
+let hobbies = ["tennis", "cooking"];
+let emptyValue = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof emptyValue);

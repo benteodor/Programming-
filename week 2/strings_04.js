e5fab77d37b92dc0f43bdda8848d07750d1b00bd
Popa Teodor <teodor.popa96@gmail.com>
2024-01-22 14:29:32 +0200

    Done

--- week 2/strings_04.js
+++ week 2/strings_04.js
@@ -1,2 +1,7 @@
-let getFirstCharacter = "Amsterdam";
-console.log(getFirstCharacter[0]);
+/*let getFirstCharacter = "Amsterdam";
+console.log(getFirstCharacter[0]);*/
+
+function getFirstCharacter(name) {
+  return name[0];
+}
+console.log(getFirstCharacter("Amsterdam"));

cf7be9df63d8b73b504022eb8323b52e2f9aad2c
Popa Teodor <teodor.popa96@gmail.com>
2024-01-23 20:18:33 +0200

    Update

---
+++ week 3/conditions_02.js
@@ -0,0 +1,1 @@
+// Create a program getTemperature that asks the user to input a temperature in Celsius and then outputs whether they should wear a jacket (if the temperature is below 15°C) or not.
---
+++ week 3/conditions_05.js
@@ -0,0 +1,12 @@
+// Complete the function evenOrOdd such that it returns the string "even" when the number parameter it receives is even and "odd" otherwise.
+
+function evenOrOdd(number) {
+  if (8 % 2) {
+    return "even";
+  } else;
+  return "odd";
+}
+
+console.log(evenOrOdd(25)); // "odd"
+console.log(evenOrOdd(18)); // "even"
+console.log(evenOrOdd(-17)); // "odd"

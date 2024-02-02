1d87df383483bb03f5d4df1bf4c765ec8b14d4b5
Popa Teodor <teodor.popa96@gmail.com>
2024-02-02 14:25:29 +0200

    Update DOM 1

--- week 4/basic DOM_1/script.js
+++ week 4/basic DOM_1/script.js
@@ -4,6 +4,12 @@ Write JavaScript code that selects content id by using getElementById.
 Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
 */
 
-document.getElementById("onclick").addEventListener("click", function () {
-  alert("Hello, World!");
-});
+const changeText = document.getElementById("content");
+
+function newText() {
+  return (changeText.textContent = "Hello, World!");
+}
+
+console.log(
+  document.getElementsByTagName("button").addEventListener("click", newText)
+);
---
+++ week 4/basic DOM_2/script2.js
@@ -0,0 +1,19 @@
+/* 
+
+Write JavaScript code that adds an event listener to the button.
+
+When the button is clicked, the function should:
+- Retrieve the value from the input field.
+- Create a new <li> element and set its text content to the input field's value.
+- Append the new <li> element to the fruitList <ul>.
+
+Ensure the input field is cleared after adding the fruit to the list.
+
+*/
+
+const inputBtn = document.getElementById("addFruitBtn");
+const inputEl = document.getElementById("fruitInput");
+function getVal() {
+  console.log(inputEl.value);
+}
+inputBtn.addEventListener("retrieve", getVal);

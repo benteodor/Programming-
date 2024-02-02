27fc14f33b4e0edef7163b9c88aaa836d3302c52
Popa Teodor <teodor.popa96@gmail.com>
2024-02-02 12:36:38 +0200

    DOM 1 done

---
+++ week 4/basic DOM_1/script.js
@@ -0,0 +1,9 @@
+/* 
+
+Write JavaScript code that selects content id by using getElementById.
+Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
+*/
+
+document.getElementById("onclick").addEventListener("click", function () {
+  alert("Hello, World!");
+});

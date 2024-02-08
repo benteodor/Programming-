9237c0bb951f9ba866371b3f4ba438e21cd60642
Popa Teodor <teodor.popa96@gmail.com>
2024-02-08 22:05:50 +0200

    demo 4

--- week 4/basic DOM_4/script4.js
+++ week 4/basic DOM_4/script4.js
@@ -1,17 +1,24 @@
 const form = document.querySelector(".form-container");
 
 const priceCalc = () => {
-  const typeSelect = document.querySelector("#type");
-  const checkboxes = document.querySelectorAll("input[type="checkbox"]");
+  // console.log('frwbfv');
 
-  let totalAmount = parent(typeSelect.value);
+  const typeSelect = document.querySelector("#type");
+  const checkboxes = document.querySelectorAll("input[type='checkbox']");
+  const priceBanner = document.getElementById("totalPrice");
+
+  // console.log(typeSelect.value);
+  // console.log(checkboxes);
+
+  let totalAmount = parseInt(typeSelect.value);
+  // console.log(totalAmount);
 
   for (const checkbox of checkboxes) {
-    if (checkbox.checkbox)
-    totalAmount += parseInt(checkbox.value);
+    if (checkbox.checked) {
+      totalAmount += parseInt(checkbox.value);
+    }
   }
-  }
+  priceBanner.textContent = "€{totalAmount}";
 };
 
 form.addEventListener("change", priceCalc);
-

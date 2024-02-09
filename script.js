2b6e35b159003e253a996dc3bf548cf2823e7c7f
Popa Teodor <teodor.popa96@gmail.com>
2024-02-09 12:34:27 +0200

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+/* let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+
+teamMembers.forEach((name) => console.log(name));
+*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+teamMembers.shift();
+console.log(teamMembers);*/
+
+let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+teamMembers.pop();
+console.log(teamMembers);

c8bab7dfa8f05679c9446f9b08dd23f122b36767
Popa Teodor <teodor.popa96@gmail.com>
2024-02-09 14:45:55 +0200

    done

--- script.js
+++ script.js
@@ -7,6 +7,56 @@ teamMembers.forEach((name) => console.log(name));
 teamMembers.shift();
 console.log(teamMembers);*/
 
-let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
 teamMembers.pop();
 console.log(teamMembers);
+*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+teamMembers.unshift("Alex");
+console.log(teamMembers);*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+teamMembers.push("Linda");
+console.log(teamMembers);*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+teamMembers.splice(0, 2);
+console.log(teamMembers);
+*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+
+console.log(teamMembers.indexOf("Mike"));*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+console.log(teamMembers.lastIndexOf("Jake"));*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+teamMembers.splice(2, 1);
+teamMembers.splice(2, 0, "Carol", "Bruce");
+console.log(teamMembers);*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+let teamMembers2 = ["Bob"];
+teamMembers3 = teamMembers.concat(teamMembers2);
+console.log(teamMembers3);
+*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+let clonedArray = teamMembers.slice();
+console.log(clonedArray);
+*/
+
+/*let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+let newMembers = ["Tina", "Dean"];
+newMembers1 = teamMembers.concat(newMembers);
+console.log(newMembers1);*/
+
+let teamMembers = ["John", "Emily", "Mike", "Sarah"];
+
+function elementCount(teamMembers, elementCount) {
+  return elementCount((currentElement) => currentElement == elementCount)
+    .length;
+}
+console.log(elementCount(teamMembers));

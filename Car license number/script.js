afc80b5852e8541b1caf1219fd62e3c594cfb850
Popa Teodor <teodor.popa96@gmail.com>
2024-02-26 14:56:10 +0200

    added class car

---
+++ Car license number/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+class car {
+  constructor(
+    licensePlate,
+    carMaker,
+    carModel,
+    currentOwner,
+    carPrice,
+    carColor
+  ) {
+    this.licensePlate = licensePlate;
+    this.carMaker = carMaker;
+    this.currentOwner = currentOwner;
+    this.carPrice = carPrice;
+    this.carColor = carColor;
+  }
+}

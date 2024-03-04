c49f9deed4826f6b10a3b599eb1483842e41fed8
Popa Teodor <teodor.popa96@gmail.com>
2024-03-04 12:40:27 +0200

    update

--- new project/data.js
+++ new project/data.js
@@ -1,8 +1,25 @@
+const header = document.querySelector("header");
+
 const backToTopButton = document.querySelector("#backToTop");
 
-const getToTop = () => {
+/* const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 };
 
 backToTopButton.addEventListener("click", getToTop);
+*/
+
+backToTopButton.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  header.style.backgroundColor = "orange";
+  header.classList.add("bg");
+  // header.classList.remove("bg");
+});
+
+/* document.querySelector("#backToTop").addEventListener("click", () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // Chrome, FF and Others
+});
+*/
